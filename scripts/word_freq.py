6908a7cf5f8f7165cd41acd8663a40c7ac8e108e
Renjie Zhu <renjie@renjie-ubuntu.attlocal.net>
2018-11-02 20:44:03 -0700

    changed way to get parent_dir

--- scripts/word_freq.py
+++ scripts/word_freq.py
@@ -11,11 +11,10 @@
 
 # be careful of the location of this file, if moved, be sure to rethink about the file paths
 
-from pathlib import Path
 import nltk
 import os
-
-parent_dir = str(Path().resolve().parent)
+print(os.getcwd())
+parent_dir = os.getcwd()
 #os.path.join joins all the arguments given in the function call
 file_data = os.path.join(parent_dir,'raw_data',input('What is the data file name? (Make sure the file is in "raw_data" folder, give file name with extension)'))
 file_out = os.path.join(parent_dir,'processed_data',input('What is the output file name? (Make sure to provide the .txt/.csv extension) '))
