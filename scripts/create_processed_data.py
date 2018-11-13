fddd3340c2d34d89b61ea89f49e0e75f11164bb3
Renjie Zhu <renjie.j.zhu@gmail.com>
2018-11-13 12:48:23 -0800

    add db functionality to create_processed_data.py

--- scripts/create_processed_data.py
+++ scripts/create_processed_data.py
@@ -1,13 +1,37 @@
+#
+# ECE 143 Group Project
+#
+# automatically process all raw data files
+#
+# by Ambareesh S J on Oct 24, 2018
+#
+# modified: 
+#   Renjie: push to database
+#
+
+from SQLite import SQLite
+import word_freq
+
 raw_data_folder_name = input('Enter raw data folder path')
 
-#raw_data_folder_name = r'C:\Users\asree\Documents\GitHub\ECE143_Project\ECE143_Project\asj\raw_data'
+# raw_data_folder_name = r'C:\Users\asree\Documents\GitHub\ECE143_Project\ECE143_Project\asj\raw_data'
+
+db = SQLite('database.db')
 
 for i in get_raw_data_filenames_in_txt_format(raw_data_folder_name):
     
-    dict_single, dict_bigram = word_freq(i)
+    dict_single, dict_bigram = word_freq.word_freq(i)
+
+    table_name = i.split('.txt')[0]
+    table_name_single = table_name + '_sinlge'
+    table_name_bigram = table_name + '_bigram'
     
-    ## TO DO : Renjie
-    #Add dict_single,dict_bigram to the Database.db in root
+    err = db.create_table(table_name_single)
+    err = db.insert_dict(table_name_single, dict_single)
+
+    err = db.create_table(table_name_bigram)
+    err = db.insert_dict(table_name_bigram, dict_bigram)
+    
 
 
 def get_raw_data_filenames_in_txt_format(raw_data_folder_name):
