70c80a99cb593189fc40b1449d2338eddb31239f
SteveLi <lidaoyu@gmail.com>
2018-11-13 11:28:30 -0800

    Separate common words into a single txt file. Now the result can store into a database file.

--- scripts/word_freq.py
+++ scripts/word_freq.py
@@ -26,7 +26,7 @@ nltk.download("wordnet")
 # parent dir for mac or linux
 parent_dir = os.getcwd()
 # parent dir for windows machine (uncomment if on windows, leave commented if elsewise)
-# parent_dir = os.path.dirname(parent_dir)
+parent_dir = os.path.dirname(parent_dir)
 # os.path.join joins all the arguments given in the function call
 file_data = os.path.join(parent_dir, 'raw_data', input(
     'What is the data file name? (Make sure the file is in "raw_data" folder, give file name with extension)'))
@@ -39,70 +39,11 @@ table_s = table_name + '_single'
 
 # keeping track of a common words list, this is a list of words that we
 # don't want in our data.
-common_words = [
-    '', ' ', 'hours of', 'of lecture', 'instructor', 'c-', 'consent of', 'consent', 'prerequisite ece', 'lecture prerequisite',
-    'and', 'of instructor',
-    'ece', 'prerequisites ece', 'lecture one', 'grades of', 'with grades',
-    'of', 'three hours',
-    'grading',
-    'study',
-    'outside',
-    'four',
-    'letter',
-    'and',
-    'ece',
-    'of',
-    'prerequisites',
-    'prerequisite',
-    'hours',
-    'lecture',
-    'three',
-    'one',
-    'discussion',
-    'hour',
-    'in',
-    'or',
-    'the',
-    'to',
-    'a',
-    'for',
-    'graduate',
-    'standing',
-    'with',
-    'will',
-    'be',
-    'course',
-    'topics',
-    'may',
-    'on',
-    'students',
-    'better',
-    'at',
-    'credit',
-    'not',
-    'by',
-    'taken',
-    'grade',
-    'is',
-    'are',
-    'ii',
-    'recommended',
-    'c-',
-    'c–',
-    'an',
-    'and/or',
-    'equivalent',
-    'this',
-    'both',
-    'from',
-    'only',
-    'faculty',
-    's/u',
-    'which',
-    'than',
-    'weekly',
-    'have',
-]  # need better algo to remove common words, I think they should't be collected in the wordlist at all, can be removed with a condition in line 91, wordlist =
+# List saved as common_words.txt
+common_words = []
+with open('common_words.txt','rt') as f:
+    for line in f.readlines():
+        common_words.append(line.strip('\n'))
 common_words.extend(list(map(chr, range(97, 123))))
 
 with open(file_data, 'r') as f:
@@ -117,9 +58,9 @@ wordlist = [i.strip(".:,();$-1234567890") for i in origlist]
 freqdict = dict(Counter(wordlist))
 newfreq = {}
 for key, value in freqdict.items():
-    if value < 3:
-        continue
-    elif key in common_words:
+    # if value < 3:
+    #     continue
+    if key in common_words:
         continue
     else:
         newfreq[key] = value
@@ -152,6 +93,8 @@ for key, value in freq_bigram.items():
         continue
     elif key[0] in common_words or key[1] in common_words:
         continue
+    # elif not (key[0] in posDict.keys() and key[1] in posDict.keys()):
+    #     continue
     elif posDict[key[0]] == 'NN' or posDict[key[0]] == 'NNS' or posDict[key[0]] == 'JJ':
         if posDict[key[1]] == 'NN' or posDict[key[1]] == 'NNS':
             picked_freq_bigram[key[0] + ' ' + key[1]] = value
