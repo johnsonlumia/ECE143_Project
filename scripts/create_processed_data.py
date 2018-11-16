5d9f6f64bc0ee83e37cc8e00a6c7622d5b0b49d0
Renjie Zhu <renjie.j.zhu@gmail.com>
2018-11-16 15:41:53 -0800

    slight changes on format

--- scripts/create_processed_data.py
+++ scripts/create_processed_data.py
@@ -5,7 +5,7 @@
 #
 # by Ambareesh S J on Oct 24, 2018
 #
-# modified: 
+# modified:
 #   Renjie: push to database
 #
 
@@ -14,34 +14,32 @@ import word_freq
 import os
 
 
+raw_data_folder_name = os.path.join(os.path.dirname(os.getcwd()), 'raw_data')
 
-raw_data_folder_name = os.path.join(os.path.dirname(os.getcwd()),'raw_data')
-
-db = SQLite('database.db')  # To do Renjie, make this parent Database in root, rather than scripts
+db = SQLite('database.db')
 
 
 def get_raw_data_filenames_in_txt_format(raw_data_folder_name):
     '''
     Generator that returns rawdata file
-    '''    
-    import os    
+    '''
     for i in os.listdir(raw_data_folder_name):
         if i.endswith('.txt'):
             yield i
 
+
 for i in get_raw_data_filenames_in_txt_format(raw_data_folder_name):
-    
-	print("Currently working on :",i) #To check status, remove later
-	dict_single, dict_bigram = word_freq.word_freq(os.path.join(raw_data_folder_name,i))
 
-	table_name = i.split('.txt')[0]
-	table_name_single = table_name + '_sinlge'
-	table_name_bigram = table_name + '_bigram'
+    print("Currently working on :", i)
+    dict_single, dict_bigram = word_freq.word_freq(
+        os.path.join(raw_data_folder_name, i))
 
-	err = db.create_table(table_name_single)
-	err = db.insert_dict(table_name_single, dict_single)
+    table_name = i.split('.txt')[0]
+    table_name_single = table_name + '_single'
+    table_name_bigram = table_name + '_bigram'
 
-	err = db.create_table(table_name_bigram)
-	err = db.insert_dict(table_name_bigram, dict_bigram)
+    err = db.create_table(table_name_single)
+    err = db.insert_dict(table_name_single, dict_single)
 
-
+    err = db.create_table(table_name_bigram)
+    err = db.insert_dict(table_name_bigram, dict_bigram)
