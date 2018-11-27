6a868e9b2bd7bdb1af0bc3bf856e3baaaabb79a2
Renjie Zhu <renjie.j.zhu@gmail.com>
2018-11-27 12:08:45 -0800

    include data analysis and visualization

--- main.py
+++ main.py
@@ -14,4 +14,13 @@ logger.setLevel(logging.INFO)
 process_data('school')
 process_data('industry')
 
+import analyze_data
+import analyze_data_department_only
+
+# Visualization
+logger.info('Visualization starts')
+import Extent_of_overlap
+import ucsd_ece_plot
+import ucsd_cse_plot
+
 logger.info('Program ends.')
